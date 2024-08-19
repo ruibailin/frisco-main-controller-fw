bd24b06b1a95ac845f8760482f512c75515c5bb7
Steve Rui <stever@a-concepts.com>
2024-08-19 11:00:12 -0500

    add perf for pmg v1.24.0.0 or later

--- new/app/scan.c
+++ new/app/scan.c
@@ -24,10 +24,20 @@ void pmg_panel2_bus_scan_task(void *in);
 	}
  *
  * ----------------------------*/
+extern void sta_main_init(void);
+extern void sta_main_loop(void);
 void pmg_scan_task(void *in);
 void pmg_scan_task(void *in)
 {
 //	Low_Level_App_Update();
+	int ss;
+	ss = eos_get_state();
+	if(ss==0)
+	{
+		sta_main_init();
+		eos_set_state(1);
+		return;
+	}
 	pmg_module_bus_scan_task(in);
 	pmg_sensor_bus_scan_task(in);
 	pmg_panel1_bus_scan_task(in);
@@ -37,6 +47,7 @@ void pmg_scan_task(void *in)
 		PowerControl_InitiateShutdown();
 	}
 	stats_task_send_records();
+	sta_main_loop();
 }
 /*================================================================*/
 /* end of scan.c */
